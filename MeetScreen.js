fbc2b79367c8833dcd47272c89d1be19165f8364
Khristian Brooks <kh.brooks02@gmail.com>
2018-07-18 16:01:48 -0400

    made request to meetup api

--- MeetScreen.js
+++ MeetScreen.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	StyleSheet,
+	ActivityIndicator,
+	Text,
+	View,
+	FlatList,
+} from 'react-native';
 
 export default class MeetScreen extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { groups: {}, isLoading: true };
+	}
+
+	componentWillMount() {
+		return fetch(
+			'https://api.meetup.com/find/groups?key=7971183234113c561618114b24513b6e'
+		)
+			.then(response => response.json())
+			.then(responseJson => {
+				this.setState({
+					groups: responseJson,
+					isLoading: false,
+				});
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}
+
 	render() {
+		const { groups, isLoading } = this.state;
+		// console.warn(groups);
+		if (this.state.isLoading) {
+			return (
+				<View style={{ flex: 1, padding: 20 }}>
+					<ActivityIndicator />
+				</View>
+			);
+		}
+
 		return (
 			<View style={styles.container}>
-				<Text>This is where we would apply the knowledge for meeting!</Text>
-				<Text> Hi Ke!!!! </Text>
+				<FlatList
+					data={this.state.groups}
+					renderItem={({ item }) => <Text>{item.name}</Text>}
+					keyExtractor={(item, index) => index}
+				/>
 			</View>
 		);
 	}
